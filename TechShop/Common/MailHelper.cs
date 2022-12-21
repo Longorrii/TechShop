147b03746120e9ee184facbd820b5eac6834aad8
Longorrii <103055943+Longorrii@users.noreply.github.com>
2022-12-21 11:40:30 +0700

    init all

---
+++ TechShop/Common/MailHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using System.Configuration;
+using System.Net;
+
+namespace TechShop.Common
+{
+    public class MailHelper
+    {
+        public void SendEmail(string toEmailAddress, string subject, string content)
+        {
+            var fromEmailAddress = ConfigurationManager.AppSettings["FromEmailAddress"].ToString();
+            var fromEmailDisplayName = ConfigurationManager.AppSettings["FromEmailDisplayName"].ToString();
+            var fromEmailPassword = ConfigurationManager.AppSettings["FromEmailPassword"].ToString();
+            string body = content;
+            toEmailAddress = "longdvnl@gmail.com";
+            MailMessage message = new MailMessage(new MailAddress(fromEmailAddress, fromEmailDisplayName), new MailAddress(toEmailAddress));
+            message.Subject = subject;
+            message.IsBodyHtml = true;
+            message.Body = body;
+            using (SmtpClient client = new SmtpClient())
+            {
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential("longdvnl@gmail.com", "ukigizxjecnavpkg");
+                client.Host = "smtp.gmail.com";
+                client.Port = 587;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Send(message);
+            }
+        }
+    }
+}
